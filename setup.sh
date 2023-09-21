2c81b8a26ce94e3eeac2f463ae6bf220bcabe089
tamshare <145654477+tamshare@users.noreply.github.com>
2023-09-21 15:10:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
+# Update package lists
 sudo apt update
+
+# Install Squid proxy server
 sudo apt install -y squid
+
+# Install Apache2 utilities
 sudo apt install -y apache2-utils
-sudo nano /etc/squid/squid.conf
